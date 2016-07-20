03a88b6bab387f5210841cf3c7906c8329cb5e1f
tikhon.osipov <tikhon.osipov@hiq.ru>
2016-07-20 17:06:00 +0300

    first commit

---
+++ app/src/main/java/com/tixon/notificationtextview/NotificationTextView.java
@@ -0,0 +1,75 @@
+package com.tixon.notificationtextview;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by tikhon.osipov on 20.07.2016
+ */
+public class NotificationTextView extends TextView {
+    Paint backgroundPaint;
+    Paint textPaint;
+    RectF rectFLeft, rectFRight;
+
+    public NotificationTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    public NotificationTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public NotificationTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public NotificationTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawBackground(canvas);
+    }
+
+    private void drawBackground(Canvas canvas) {
+        //canvas.drawCircle(getWidth()/2, getHeight()/2, 4, backgroundPaint);
+        //left arc
+        canvas.drawArc(new RectF(0, 0, getHeight(), getHeight()), 90, 180, false, backgroundPaint);
+        //right arc
+        canvas.drawArc(new RectF(getWidth() - getHeight(), 0, getWidth(), getHeight()), -90, 180, false, backgroundPaint);
+        //middle rect
+        if(getWidth() > getHeight()) {
+            canvas.drawRect(new RectF(getHeight()/2, 0, getWidth() - getHeight()/2, getHeight()), backgroundPaint);
+        }
+        canvas.drawText(getText(), 0, getText().length(), getWidth()/2 - 8, getHeight()/2 + 10, textPaint);
+    }
+
+    private void init() {
+        backgroundPaint = new Paint();
+        backgroundPaint.setAntiAlias(true);
+        backgroundPaint.setStyle(Paint.Style.FILL);
+        backgroundPaint.setColor(Color.parseColor("#2196f3"));
+
+        textPaint = new Paint();
+        textPaint.setAntiAlias(true);
+        textPaint.setColor(Color.parseColor("#ffffff"));
+        textPaint.setTextSize(Utils.dpToPx(11, getContext()));
+
+        rectFLeft = new RectF(0, 0, getHeight()/2, getHeight());
+        rectFRight = new RectF(getWidth() - getHeight()/2, 0, getWidth(), getHeight());
+    }
+}
